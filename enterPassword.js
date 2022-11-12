2fe472212e1ab5338be90a0b45e331c7078b6264
Vasilkova Lyubov <damirana_@mail.ru>
2022-11-13 01:02:33 +0700

    Итоговый вариант программы

--- enterPassword.js
+++ enterPassword.js
@@ -1,13 +1,6 @@
-const { futimesSync } = require('fs');
-const readline = require( 'readline' );
-const rl = readline.createInterface(process.stdin, process.stdout)
-
-const password1 = 'klever2000'
-const password2 = 'albumin555'
-const password3 = 'knopkazapuskaraket'
-
 function getPasswordChecker (password) {
     console.log(`пароль: ${password}`)
+    // return answer => password == answer
     return function check(answer) {
         console.log(`пользовательский ввод: ${answer}`)
         if (password == answer) {
@@ -15,25 +8,13 @@ function getPasswordChecker (password) {
             return true        
         } else {
             console.log('Вы ввели не верный пароль, попробуйте еще раз.')
-            getPasswordChecker (password)
             return false
         }
     }
-    
 }
 
-let p = getPasswordChecker(password1)
-p('партизаны не сдаются')
-p('долой php')
-p('не вешать нос.гардемарины.com')
-p('klever2000')
-
-// let p2 = getPasswordChecker(password2)
-// p2('гарандапогала')
-// p2('Пырышки-пупырышки')
-// p2('albumin555')
-
-// let p3 = getPasswordChecker(password3)
-// p3('gfggiyf')
-// p3('knopkazapuskaraket')
-
+let p = getPasswordChecker('klever2000')
+console.log(Boolean(p('партизаны не сдаются')))
+console.log(Boolean(p('долой php')))
+console.log(Boolean(p('не вешать нос.гардемарины.com')))
+console.log(Boolean(p('klever2000')))
