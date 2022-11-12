33f8e7b789e18f8bf34756cd395225c9233af08d
Vasilkova Lyubov <damirana_@mail.ru>
2022-11-12 12:13:14 +0700

    Доработка задачи

--- enterPassword.js
+++ enterPassword.js
@@ -1,3 +1,4 @@
+const { futimesSync } = require('fs');
 const readline = require( 'readline' );
 const rl = readline.createInterface(process.stdin, process.stdout)
 
@@ -6,18 +7,20 @@ const password2 = 'albumin555'
 const password3 = 'knopkazapuskaraket'
 
 function getPasswordChecker (password) {
-    rl.question('Введите пароль: ', (answer) => {
-        console.log('Вы ввели:', answer)
-        if (answer == password) {
-            console.log('Вы ввели верный парооль!')
-            rl.close()
-            return true
-        } else {
-            console.log('Вы ввели не верный пароль, попробуйте еще раз.')
-            getPasswordChecker(password)
-            return false
-        }
-    });
+    rl.question('Введите пароль: ', checkPassword)  
+}
+
+function checkPassword(answer) {
+    console.log('Вы ввели:', answer);
+    if (password1 == answer) {
+        console.log('Вы ввели верный парооль!')
+        rl.close();
+        return true        
+    } else {
+        console.log('Вы ввели не верный пароль, попробуйте еще раз.')
+        getPasswordChecker (password1)
+        return false
+    }
 }
 
 getPasswordChecker(password1)
